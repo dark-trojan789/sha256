485b135bf60416392dbb4199210bd42ba2538ae6
dark-trojan789 <108808853+dark-trojan789@users.noreply.github.com>
2024-12-30 22:16:40 +0530

    Update tb_sha256.py

--- tb/tb_sha256.py
+++ tb/tb_sha256.py
@@ -5,27 +5,27 @@ from cocotb.triggers import RisingEdge, Timer, ReadOnly
 def sb_fn(actual_value, expected_value):
     assert actual_value == expected_value, f"Mismatch: Expected {hex(expected_value)}, got {hex(actual_value)}"
 
-# @cocotb.test
-# async def sha256_test(dut):
-#     print(dir(dut))
-#     tc1 = 0x61626380000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000018
-#     expected_res1 = 0xBA7816BF8F01CFEA414140DE5DAE2223B00361A396177A9CB410FF61F20015AD
+@cocotb.test
+async def sha256_test(dut):
+     print(dir(dut))
+     tc1 = 0x61626380000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000018
+     expected_res1 = 0xBA7816BF8F01CFEA414140DE5DAE2223B00361A396177A9CB410FF61F20015AD
 
-#     dut.message.value = tc1
-#     dut.block = 1
+     dut.message.value = tc1
+     dut.block = 1
     
-#     dut.rst.value = 1
-#     await Timer(5, 'ns')
-#     dut.rst.value = 0
-#     #await Timer(, 'ns') 
+     dut.rst.value = 1
+     await Timer(5, 'ns')
+     dut.rst.value = 0
+     #await Timer(, 'ns') 
 
-#     await RisingEdge(dut.hash_valid)
+     await RisingEdge(dut.hash_valid)
 
-#     await ReadOnly()
-#     actual_val = dut.hash.value.integer
-#     #cocotb.log.info(f" {actual_val} ")
-#     await Timer(20, 'ns')
-#     sb_fn(actual_val, expected_res1)
+     await ReadOnly()
+     actual_val = dut.hash.value.integer
+     #cocotb.log.info(f" {actual_val} ")
+     await Timer(20, 'ns')
+     sb_fn(actual_val, expected_res1)
 
 @cocotb.test
 async def sha256_test_2_blocks(dut):
